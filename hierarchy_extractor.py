0e87ba70b7a0a6ad9eeee09060ea000e83862da9
drschwenk <schwgit2@gmail.com>
2016-08-03 13:05:52 -0700

    Merged t/f questions now follow proper schema

--- hierarchy_extractor.py
+++ hierarchy_extractor.py
@@ -9,7 +9,7 @@ import pprint
 
 
 class QuestionTypeParser(object):
-    def __init__(self, overlap_tol, blank_threshold):
+    def __init__(self, overlap_tol=None, blank_threshold=None):
         self.numeric_starters = [str(n) + '.' for n in range(20)]
         self.letter_starters = [char + '.' for char in string.ascii_uppercase[:6]]
         self.letter_starters += [char + '.' for char in string.ascii_lowercase[:6]]
@@ -31,20 +31,47 @@ class QuestionTypeParser(object):
             return FillInBlankParser()
 
     @classmethod
+    def start_x(cls, box):
+        return box['rectangle'][0][0]
+
+    @classmethod
+    def start_y(cls, box):
+        return box['rectangle'][0][1]
+
+    @classmethod
+    def end_x(cls, box):
+        return box['rectangle'][1][0]
+
+    @classmethod
+    def end_y(cls, box):
+        return box['rectangle'][1][1]
+
+    @classmethod
+    def height(cls, box):
+        return QuestionTypeParser.end_y(box) - QuestionTypeParser.start_y(box)
+
+    @classmethod
+    def width(cls, box):
+        return QuestionTypeParser.end_x(box) - QuestionTypeParser.start_x(box)
+
+    @classmethod
     def clean_box(cls, box):
-        box_copy = deepcopy(box)
-        del box_copy['source']
-        del box_copy['score']
-        del box_copy['v_dim']
-        del box_copy['category']
-        del box_copy['group_n']
-        return box_copy
+        if 'source' in box.keys():
+            box_copy = deepcopy(box)
+            del box_copy['source']
+            del box_copy['score']
+            del box_copy['v_dim']
+            del box_copy['category']
+            del box_copy['group_n']
+            return box_copy
+        else:
+            return box
 
     @classmethod
     def get_last_added(cls, ordered_prop_dict, depth):
         return functools.reduce(lambda x, _: list(x.items())[-1][1], range(depth), ordered_prop_dict)
 
-    def merge_boxes(self, sorted_box_groups):
+    def merge_boxes(self, sorted_box_groups, category):
         min_x = min(map(lambda x: QuestionTypeParser.start_x(x), sorted_box_groups))
         max_x = max(map(lambda x: QuestionTypeParser.end_x(x), sorted_box_groups))
         min_y = min(map(lambda x: QuestionTypeParser.start_y(x), sorted_box_groups))
@@ -53,15 +80,37 @@ class QuestionTypeParser(object):
         combined_words = self.blank_signifier.join(map(lambda x: x['contents'], sorted_box_groups))
         combined_rect = [[min_x, min_y], [max_x, max_y]]
         new_box = {
+            'category': category,
             'contents': combined_words,
             'box_id': combined_box_id,
             'rectangle': combined_rect}
         return new_box
 
+    def append_box_to_last_element(self, box, ask_index, category):
+        last_val = QuestionTypeParser.get_last_added(self.parsed_questions, 3)
+        last_key, _ = self.parsed_questions.popitem()
+        combined_box = self.merge_boxes([last_val, box], category)
+        _, starting_chars = self.check_starting_chars(combined_box['contents'])
+        self.make_question_component(combined_box, ask_index, starting_chars)
+        # self.parsed_questions[last_key] = combined_box
+
+    def detect_blank(self, current_box, parsed_questions):
+        last_box_seen = list(QuestionTypeParser.get_last_added(parsed_questions, 2).values())[0]
+        large_gap = (QuestionTypeParser.start_x(current_box) - QuestionTypeParser.end_x(
+            last_box_seen)) > self.blank_thresh
+        same_line = self.check_for_same_line(current_box, last_box_seen)
+        return same_line and large_gap
+
+    def check_for_same_line(self, current_box, last_box_seen):
+        boxes_y_coords = [[QuestionTypeParser.start_y(current_box), QuestionTypeParser.end_y(current_box)],
+                          [QuestionTypeParser.start_y(last_box_seen), QuestionTypeParser.end_y(last_box_seen)]]
+        ordered_boxes = sorted(boxes_y_coords)
+        overlap = max(0, min(ordered_boxes[0][1], ordered_boxes[1][1]) - max(ordered_boxes[0][0], ordered_boxes[1][0]))
+        larger_box_width = max(QuestionTypeParser.height(current_box), QuestionTypeParser.height(last_box_seen))
+        return overlap / float(larger_box_width) > self.overlap_tol
+
     def make_instruction_component(self, inst_id, box):
-        self.parsed_questions['instructions'] = {
-            "instruction": {'I' + str(inst_id): QuestionTypeParser.clean_box(box)},
-        }
+        self.parsed_questions['instructions'] = {'inst_line_' + str(inst_id): QuestionTypeParser.clean_box(box)},
 
     def make_question_component(self, box, ask_index, structural_id):
         box_category = box['category']
@@ -89,54 +138,6 @@ class QuestionTypeParser(object):
         else:
             return None, None
 
-    def append_box_to_last_element(self, box):
-        last_val = QuestionTypeParser.get_last_added(self.parsed_questions, 3)
-        last_key, _ = self.parsed_questions.popitem()
-        combined_box = self.merge_boxes([last_val, box])
-        # print(last_val)
-        # print(box)
-        # print(combined_box)
-        # print('')
-        self.parsed_questions[last_key] = combined_box
-
-    @classmethod
-    def start_x(cls, box):
-        return box['rectangle'][0][0]
-
-    @classmethod
-    def start_y(cls, box):
-        return box['rectangle'][0][1]
-
-    @classmethod
-    def end_x(cls, box):
-        return box['rectangle'][1][0]
-
-    @classmethod
-    def end_y(cls, box):
-        return box['rectangle'][1][1]
-
-    @classmethod
-    def height(cls, box):
-        return QuestionTypeParser.end_y(box) - QuestionTypeParser.start_y(box)
-
-    @classmethod
-    def width(cls, box):
-        return QuestionTypeParser.end_x(box) - QuestionTypeParser.start_x(box)
-
-    def check_for_same_line(self, current_box, last_box_seen):
-        boxes_y_coords = [[QuestionTypeParser.start_y(current_box), QuestionTypeParser.end_y(current_box)],
-                          [QuestionTypeParser.start_y(last_box_seen), QuestionTypeParser.end_y(last_box_seen)]]
-        ordered_boxes = sorted(boxes_y_coords)
-        overlap = max(0, min(ordered_boxes[0][1], ordered_boxes[1][1]) - max(ordered_boxes[0][0], ordered_boxes[1][0]))
-        larger_box_width = max(QuestionTypeParser.height(current_box), QuestionTypeParser.height(last_box_seen))
-        return overlap / float(larger_box_width) > self.overlap_tol
-
-    def detect_blank(self, current_box, parsed_questions):
-        last_box_seen = list(QuestionTypeParser.get_last_added(parsed_questions, 2).values())[0]
-        large_gap = (QuestionTypeParser.start_x(current_box) - QuestionTypeParser.end_x(last_box_seen)) > self.blank_thresh
-        same_line = self.check_for_same_line(current_box, last_box_seen)
-        return same_line and large_gap
-
 
 class MultipleChoiceParser(QuestionTypeParser):
     def __init__(self):
@@ -149,7 +150,7 @@ class MultipleChoiceParser(QuestionTypeParser):
         return box_text
 
     def make_answer_choice(self, box, structural_id):
-        choice_id = 'answer_choice ' + structural_id
+        choice_id = structural_id
         question_id = 'Q_' + str(self.current_question_number)
         if 'answer_choices' not in self.parsed_questions[question_id].keys():
             self.parsed_questions[question_id]['answer_choices'] = OrderedDict()
@@ -194,7 +195,7 @@ class TrueFalseParser(QuestionTypeParser):
                 self.current_question_number += 1
                 self.make_question_component(box, ask_index, starting_chars)
             elif self.detect_blank(box, self.parsed_questions):
-                self.append_box_to_last_element(QuestionTypeParser.clean_box(box))
+                self.append_box_to_last_element(QuestionTypeParser.clean_box(box), ask_index, box['category'])
             elif start_type == 'letter start':
                 pass
 
@@ -303,11 +304,10 @@ class PageQuestionParser(object):
     @classmethod
     def parse_page_questions(cls, all_page_boxes, overlap_tol, blank_threshold):
         question_types = ['Multiple Choice', 'Short Answer', 'True/False', 'Fill-In-the-Blank']
-        ordered_questions = sorted(all_page_boxes['question'].values(),
-                                   key=lambda x: (x['rectangle'][0][1], x['rectangle'][0][0]))
+        # ordered_questions = sorted(all_page_boxes['question'].values(),
+        #                            key=lambda x: (x['rectangle'][0][1], x['rectangle'][0][0]))
         ordered_questions = PageQuestionParser.fuzzy_sort(all_page_boxes, 10)
-        # pprint.pprint(ordered_questions)
-        questions_by_type = {qt: [box for box in ordered_questions if box['category'] == qt] for qt in question_types[2:3]}
+        questions_by_type = {qt: [box for box in ordered_questions if box['category'] == qt] for qt in question_types[:3]}
 
         master_page_parser = QuestionTypeParser(overlap_tol, blank_threshold)
         for q_type, question_boxes in questions_by_type.items():
