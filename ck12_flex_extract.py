195284421539021ffe9567aad387aab34d7a8e3f
drschwenk <schwgit2@gmail.com>
2016-08-16 09:04:47 -0700

    transferring after trip

--- ck12_flex_extract.py
+++ ck12_flex_extract.py
@@ -4,6 +4,12 @@ from collections import defaultdict
 # TODO color and font definitions for sections
 # TODO detect headers and assemble topics
 
+section_demarcations = {
+    'topic_color': (0.811767578125, 0.3411712646484375, 0.149017333984375),
+    'lesson_size': 22.3082,
+    'chapter_size': 0
+}
+
 
 def parse_pdf(file_path):
     doc = pdf.Document(file_path)
@@ -26,6 +32,10 @@ def extract_page_text(page):
                     'font_size': list(line.char_fonts)[0].size,
                     'font_color': list(line.char_fonts)[0].color.as_tuple()
                 }
+                if line_dict['font_size'] == section_demarcations['lesson_size']:
+                    print 'lesson'
+                elif line_dict['font_color'] == section_demarcations['topic_color']:
+                    print 'topic'
                 if sum(line_dict['font_color']) or True:
                     page_dict['P' + str(page.page_no)].update({'T' + str(box_index): line_dict})
                     box_index += 1
