937e8466085de95f6cfbd558ea7300925f4a8ebe
drschwenk <schwgit2@gmail.com>
2016-08-18 16:46:53 -0700

    extracting quiz titles from docxs

--- ck12_flex_extract.py
+++ ck12_flex_extract.py
@@ -1,7 +1,8 @@
 import pdfparser.poppler as pdf
 from collections import defaultdict
+import glob
+import numpy as np
 
-# TODO color and font definitions for sections
 # TODO detect headers and assemble topics
 
 
@@ -28,15 +29,81 @@ class CK12FlexbookParser(object):
         for flow in page:
             for block in flow:
                 for line in block:
-                    line_dict = {
+                    line_props = {
                         'content': line.text,
                         'rectangle': line.bbox.as_tuple(),
                         'font_size': list(line.char_fonts)[0].size,
                         'font_color': list(line.char_fonts)[0].color.as_tuple()
                     }
-                    if line_dict['font_size'] == self.section_demarcations['lesson_size']:
-                        self.current_lesson = line_dict['content']
-                    elif line_dict['font_color'] == self.section_demarcations['topic_color']:
-                        self.current_topic = line_dict['content']
-                    elif not sum(line_dict['font_color']) and self.current_topic:
-                        self.parsed_content[self.current_lesson][self.current_topic] += line_dict['content'] + '\n'
+                    if line_props['font_size'] == self.section_demarcations['lesson_size']:
+                        self.current_lesson = line_props['content']
+                    elif line_props['font_color'] == self.section_demarcations['topic_color']:
+                        self.current_topic = line_props['content']
+                    elif not sum(line_props['font_color']) and self.current_topic:
+                        self.parsed_content[self.current_lesson][self.current_topic] += line_props['content'] + '\n'
+
+
+class CK12QuizParser(object):
+
+    def __init__(self):
+        self.current_lesson = None
+        self.current_topic = None
+        self.parsed_content = defaultdict(lambda: defaultdict(str))
+        self.color_demarcations = {
+            'black': (1, 0, 0),
+            'title_color': (0.0901947021484, 0.211761474609, 0.364700317383)
+        }
+        self.font_demarcations = {
+            'standard_size': 12.0,
+            'answer_choice': 10.8
+        }
+        self.stop_words = {
+            'titles': [u'answers', u'quiz', u'answer', u'key']
+        }
+        self.titles = []
+
+    def check_color(self, line_properties):
+            for k, v in self.color_demarcations.items():
+                if np.isclose(v, line_properties['color']).min():
+                    return k
+            else:
+                return None
+
+    def parse_pdf(self, file_path):
+        doc = pdf.Document(file_path)
+        for page in doc:
+            self.extract_page_text(page)
+        return self.parsed_content
+
+    def extract_page_text(self, page):
+        for flow in page:
+            for block in flow:
+                for line in block:
+                    line_props = {
+                        'content': line.text,
+                        'rectangle': line.bbox.as_tuple(),
+                        'font_size': list(line.char_fonts)[0].size,
+                        'color': list(line.char_fonts)[0].color.as_tuple()
+                    }
+                    if self.check_color(line_props) == 'title_color':
+                        title_text = line_props['content'].lower()
+                        for sw in self.stop_words['titles']:
+                            title_text = title_text.replace(sw, '')
+                        self.titles.append(title_text.strip().encode('ascii', 'ignore'))
+                    # self.sizes_seen.append(line_props['font_size'])
+                    # self.colors_seen.append(line_props['font_color'])
+                    # if line_props['font_size'] == self.section_demarcations['lesson_size']:
+                    #     self.current_lesson = line_props['content']
+                    # elif line_props['font_color'] == self.section_demarcations['topic_color']:
+                    #     self.current_topic = line_props['content']
+                    # elif not sum(line_props['font_color']) and self.current_topic:
+                    #     self.parsed_content[self.current_lesson][self.current_topic] += line_props['content'] + '\n'
+
+    def parse_pdf_collection(self, pdf_dir):
+        for pdf_file in glob.glob(pdf_dir + '/*'):
+        # for pdf_file in glob.glob(pdf_dir + '/*')[:3]:
+            self.parse_pdf(pdf_file)
+        return self.titles
+
+
+
